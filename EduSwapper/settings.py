f96330292ee25a474054bf1776277cab4982cc01
Gvantsie <g_euashvili@cu.edu.ge>
2024-07-05 15:15:45 +0400

    implementing restapi-framework

--- EduSwapper/settings.py
+++ EduSwapper/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,9 +37,23 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'rest_framework_simplejwt',
     'user',
     'matching',
+    'messaging',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -73,7 +86,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'EduSwapper.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -83,7 +95,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -103,7 +114,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -114,7 +124,6 @@ TIME_ZONE = 'Asia/Tbilisi'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
--- user/models/user.py
+++ user/models/user.py
@@ -7,8 +7,11 @@ from user.user_choices.country_choices import COUNTRY_CHOICES
 
 
 class User(AbstractUser):
-    skills = models.ManyToManyField('Skill', related_name='users_with_skill', verbose_name=_("Skills"))
-    interests = models.ManyToManyField('Interest', related_name='users_with_interest', verbose_name=_("Interests"))
+    bio = models.TextField(blank=True, null=True, verbose_name=_("Bio"))
+    birth_date = models.DateField(blank=True, null=True, verbose_name=_("Birth Date"))
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True,
+                                        verbose_name=_("Profile Picture"))
+    date_joined = models.DateTimeField(auto_now_add=True, verbose_name=_("Date Joined"))
 
     def __str__(self):
         return self.username
@@ -21,12 +24,9 @@ class User(AbstractUser):
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='profile',
                                 verbose_name=_("User"))
-    bio = models.TextField(blank=True, null=True, verbose_name=_("Bio"))
+    skills = models.ManyToManyField('Skill', related_name='users_with_skill', verbose_name=_("Skills"))
+    interests = models.ManyToManyField('Interest', related_name='users_with_interest', verbose_name=_("Interests"))
     country = models.CharField(max_length=2, choices=COUNTRY_CHOICES, blank=True, null=True, verbose_name=_("Country"))
-    birth_date = models.DateField(blank=True, null=True, verbose_name=_("Birth Date"))
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True,
-                                        verbose_name=_("Profile Picture"))
-    date_joined = models.DateTimeField(auto_now_add=True, verbose_name=_("Date Joined"))
 
     def __str__(self):
         return self.user.username
